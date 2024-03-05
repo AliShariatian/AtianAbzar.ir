44ccb5d67195e5725f8a9c044fd5a8f250fd2824
Ali Shariatian <shariatian92@gmail.com>
2024-03-06 02:16:13 +0330

    💄 add arrow down

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const RootLayout: FC<PropsWithChildren> = ({ children }): JSX.Element => {
    return (
       <html lang="fa" dir="rtl">
          <body
-            className={`${danaFont.variable} ${morabbaFont.variable} ${jetBrainsFont.variable} bgDotPattern relative overflow-x-hidden bg-slate-100 py-32 font-dana text-slate-700 transition-all duration-200 ease-in xl:pt-40`}
+            className={`${danaFont.variable} ${morabbaFont.variable} ${jetBrainsFont.variable} bgDotPattern relative overflow-x-hidden bg-slate-100 py-28 font-dana text-slate-700 transition-all duration-200 ease-in xl:pt-40`}
          >
             <PageLoadingProgressBar />
             <Navbar />
--- src/components/tools/ToolsPanel.tsx
+++ src/components/tools/ToolsPanel.tsx
@@ -1,12 +1,17 @@
 import { FC } from "react";
 import ToolBox from "./ToolBox";
 import { tools, ToolsType } from "@/utils/tools";
+import arrowDownIcon from "../../../public/img/arrow-down.svg";
+import Image from "next/image";
 
 const ToolsPanel: FC = (): JSX.Element => {
    return (
       <div id="toolbox" className="flex w-11/12 flex-col justify-center">
          {/* Title */}
-         <h2 className="mx-auto mb-16 animate-bounce text-4xl font-bold">لیست ابزارها</h2>
+         <div className="mx-auto mb-16 flex flex-col items-center">
+            <h2 className="text-4xl font-bold">لیست ابزارها</h2>
+            <Image src={arrowDownIcon} width={30} height={30} alt="arrow down" className="mt-6 animate-bounce opacity-80" />
+         </div>
 
          {/* Tools list */}
          <div dir="ltr" className="grid w-full grid-cols-1 gap-4 rounded-xl border-2 border-dashed bg-slate-100 p-12 xl:grid-cols-5">
