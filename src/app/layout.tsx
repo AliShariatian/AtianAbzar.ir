fe13631477a930cbfdbb10b01d39a70952288174
Ali Shariatian <shariatian92@gmail.com>
2024-03-06 22:34:17 +0330

    💄 style: change main page padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const RootLayout: FC<PropsWithChildren> = ({ children }): JSX.Element => {
    return (
       <html lang="fa" dir="rtl">
          <body
-            className={`${danaFont.variable} ${morabbaFont.variable} ${jetBrainsFont.variable} bgDotPattern relative overflow-x-hidden bg-slate-100 py-28 font-dana text-slate-700 transition-all duration-200 ease-in`}
+            className={`${danaFont.variable} ${morabbaFont.variable} ${jetBrainsFont.variable} bgDotPattern relative overflow-x-hidden bg-slate-100 py-36 font-dana text-slate-700 transition-all duration-200 ease-in`}
          >
             <PageLoadingProgressBar />
             <Navbar />
