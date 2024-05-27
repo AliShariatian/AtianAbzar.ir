aa78bf517df57566d7554df4fa53298265c1b77b
Ali Shariatian <shariatian92@gmail.com>
2024-05-27 21:45:46 +0330

    💄 style: add separate number to Output component

--- src/components/tools/Output.tsx
+++ src/components/tools/Output.tsx
@@ -13,7 +13,7 @@ const Output: FC<TProps> = ({ value, description, borderColor, unit }): JSX.Elem
       <div className={`${borderColor} ${borderColor ? "ring-4" : null} userHandle flex-row-reverse font-medium`}>
          <div className="flex flex-row-reverse">
             {unit && <label className="mr-4 flex w-7 items-center whitespace-nowrap text-right text-sm">{unit}</label>}
-            <div className="text-left">{value}</div>
+            <div className="text-left">{value.toLocaleString()}</div>
          </div>
 
          {description && <span className="text-lg">{description}</span>}
