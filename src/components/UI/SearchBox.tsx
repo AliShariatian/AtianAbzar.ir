3f38bd6216ddc249487cab1955f0fdb03d8fc55b
Ali Shariatian <shariatian92@gmail.com>
2024-06-06 23:11:43 +0330

    💄 style: change search box bg color

--- src/components/UI/SearchBox.tsx
+++ src/components/UI/SearchBox.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const SearchBox: FC = (): JSX.Element => {
    return (
-      <div className="userHandle h-11 w-96 py-0 transition-shadow hover:shadow-md max-xl:hidden">
+      <div className="userHandle h-11 !w-96 border-2 border-slate-200 !py-0 !shadow-none transition-shadow hover:shadow max-xl:hidden">
          <input type="search" placeholder="جستجوی ابزارها..." maxLength={30} className="size-full bg-transparent outline-none" />
       </div>
    );
