738aa5e04847cadd194f0c9df443d335bb363fee
Ali Shariatian <shariatian92@gmail.com>
2024-06-08 00:37:08 +0330

    💄 style: hide version number in mobile

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 const Footer: FC = (): JSX.Element => {
    return (
       <nav className="z-40 flex w-full justify-center pb-7 pt-72">
-         <div className="flex flex-col items-center justify-between gap-y-4 *:cursor-default *:rounded-lg *:border *:border-slate-300 *:bg-slate-50 *:px-3 *:py-1 xl:w-11/12 xl:flex-row">
+         <div className="flex items-center justify-between gap-y-4 *:cursor-default *:rounded-lg *:border *:border-slate-300 *:bg-slate-50 *:px-3 *:py-1 sm:w-11/12">
             <p>
                طراحی و توسعه توسط&nbsp;
                <Link href="https://shariatian.ir" target="_blank" title="رفتن به صفحه سازنده" className="font-bold hover:underline">
@@ -17,7 +17,7 @@ const Footer: FC = (): JSX.Element => {
                </Link>
             </p>
 
-            <span>نسخه {appVersionNumberInFooter}</span>
+            <span className="max-sm:hidden">نسخه {appVersionNumberInFooter}</span>
          </div>
       </nav>
    );
