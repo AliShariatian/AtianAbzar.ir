f6001b140303e8ddc7fb1066baa6c7954f460c1a
Ali Shariatian <shariatian92@gmail.com>
2024-06-15 16:31:16 +0330

    🐛 fix: add dark color to page top loader

--- src/components/UI/PageLoadingProgressBar.tsx
+++ src/components/UI/PageLoadingProgressBar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { FC } from "react";
 import NextTopLoader from "nextjs-toploader";
+import { useTheme } from "next-themes";
 
 const PageLoadingProgressBar: FC = (): JSX.Element => {
-   return <NextTopLoader color="#0f172a" shadow={false} height={3} showSpinner={false} />;
+   const { theme } = useTheme();
+
+   return <NextTopLoader color={theme == "dark" ? "#4372df" : "#0f172a"} shadow={false} height={3} showSpinner={false} />;
 };
 
 export default PageLoadingProgressBar;
