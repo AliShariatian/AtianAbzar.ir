5500dd97a3e17c9f3f2e98bd62764acc0a23fa0d
Ali Shariatian <shariatian92@gmail.com>
2024-06-19 16:28:51 +0330

    🐛 fix: add overflow-hidden when output in big number

--- src/components/tools/Output.tsx
+++ src/components/tools/Output.tsx
@@ -11,18 +11,16 @@ type TProps = {
 const Output: FC<TProps> = ({ value, description, borderColor, unit, isRtl = false }): JSX.Element => {
    return (
       <div className={`${borderColor} ${borderColor ? "ring-4" : null} userHandle`}>
-         <div className="flex w-full flex-col-reverse gap-2">
+         {/* Description */}
+         {description && <span className="mb-auto ml-6 w-fit text-nowrap font-semibold">{description}:</span>}
+         <div className="flex w-full flex-row-reverse overflow-hidden">
+            {/* Unit */}
+            {unit && <label className="mr-2 flex items-center whitespace-nowrap text-right text-sm">{unit}</label>}
+
             {/* Result */}
-            <div className="flex w-full flex-row-reverse">
-               {unit && <label className="mr-2 flex items-center whitespace-nowrap text-right text-sm">{unit}</label>}
-
-               <div dir={isRtl ? "rtl" : "ltr"} className={`${isRtl ? " text-right" : "text-left"} my-auto w-full select-all overflow-hidden text-wrap`}>
-                  {isRtl ? (value as string) : value?.toLocaleString()}
-               </div>
+            <div dir={isRtl ? "rtl" : "ltr"} className={`${isRtl ? "text-right" : "text-left"} my-auto select-all bg-transparent`}>
+               {isRtl ? (value as string) : value?.toLocaleString()}
             </div>
-
-            {/* Description */}
-            {description && <span className="ml-6 w-fit font-semibold">{description}:</span>}
          </div>
       </div>
    );
